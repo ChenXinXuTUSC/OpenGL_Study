4cf9af347b35139b7fa0d566cfab2e75a4945e8d
Karen-Cyber <1031307782@qq.com>
2021-09-08 23:46:59 +0800

    common study update

--- app/texture.cpp
+++ app/texture.cpp
@@ -50,7 +50,7 @@ int main()
 
     // build and compile our shader zprogram
     // ------------------------------------
-    Shader ourShader("./source/shader/vshader.glsl", "./source/shader/fshader.glsl");
+    Shader ourShader("../source/shader/vshader.glsl", "../source/shader/fshader.glsl");
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
@@ -106,7 +106,7 @@ int main()
     stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
     // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; 
     // replace it with your own image path.
-    unsigned char *data = stbi_load(std::string("images/Lena.png").c_str(), &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load(std::string("../images/Lena.png").c_str(), &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
@@ -128,7 +128,7 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load image, create texture and generate mipmaps
-    data = stbi_load(std::string("images/Flower.jpg").c_str(), &width, &height, &nrChannels, 0);
+    data = stbi_load(std::string("../images/Flower.jpg").c_str(), &width, &height, &nrChannels, 0);
     if (data)
     {
         // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
